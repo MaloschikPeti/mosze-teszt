27941fd4b8ec8b923f7e51d5cba735db8faf8c0b
MaloschikPeter <maloschikpeter@gmail.com>
2024-12-27 17:33:56 +0100

    Hibák kijavítása

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int* b = new int[NELEMENTS];//helytelen változó név, hiányzó _ jel
-    std::cout << '1-100 ertekek duplazasa'//helytelen karakter használat a string kezeléshez ' helyett " kéne, hiányzik a sortörés
-        for (int i = 0;)//helytelen for ciklus feltétel csak a kezdõérték van megadva
-        {
-            b[i] = i * 2;//helytelen módon van használva az i iterátor, nem 1-100 ig megy a sor, hanem 0-99-ig
-        }
-    for (int i = 0; i; i++)//hiányzik a feltételbõl, hogy az i-nek meddig kell növekednie
+    int* b = new int[N_ELEMENTS];//helytelen változó név, hiányzó _ jel
+    std::cout << "1-100 ertekek duplazasa" << std::endl;//helytelen karakter használat a string kezeléshez ' helyett " kéne, hiányzik a sortörés
+    for (int i = 0; i < 100; i++)//helytelen for ciklus feltétel csak a kezdõérték van megadva
     {
-        std::cout << "Ertek:"//hiányzik a b[i] és a sortörés
+        b[i] = (i + 1) * 2;//helytelen módon van használva az i iterátor, nem 1-100 ig megy a sor, hanem 0-99-ig
+    }
+    for (int i = 0; i < 100; i++)//hiányzik a feltételbõl, hogy az i-nek meddig kell növekednie
+    {
+        std::cout << "Ertek: " << b[i] << std::endl;//hiányzik a b[i] és a sortörés
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;//hiányzik a kezdõérték definiálása
-    for (int i = 0; i < N_ELEMENTS, i++)//, helyett ; kéne használni
+    int atlag = 0;//hiányzik a kezdõérték definiálása
+    for (int i = 0; i < N_ELEMENTS; i++)//, helyett ; kéne használni
     {
-        atlag += b[i]//hiányzik a sorvégi ; jel
+        atlag += b[i];//hiányzik a sorvégi ; jel
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
